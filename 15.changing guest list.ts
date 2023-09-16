343b2a6c60819b581addb957eb862c1b43af3968
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:37:53 +0500

    Add files via upload

---
+++ 15.changing guest list.ts
@@ -0,0 +1,21 @@
+// Create a list of people to invite to dinner
+const dinnerGuests: string[] = ["Alia", "Umer", "Areeb"];
+
+// Find out that one of the guests can't make it
+const guestWhoCantMakeIt: string = "Umer";
+
+// Remove the guest who can't make it
+const indexOfGuestWhoCantMakeIt: number = dinnerGuests.indexOf(guestWhoCantMakeIt);
+
+if (indexOfGuestWhoCantMakeIt !== -1) {
+  dinnerGuests.splice(indexOfGuestWhoCantMakeIt, 1);
+}
+
+// Add a new guest to the list
+const newGuest: string = "Zara";
+dinnerGuests.push(newGuest);
+
+// Send out new invitations
+dinnerGuests.forEach((guest) => {
+  console.log(`Dear ${guest}, you are invited to dinner at my place this Saturday at 7 PM. We hope to see you there!`);
+});
