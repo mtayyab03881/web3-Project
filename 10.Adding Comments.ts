e290578a104ff6add620f0c798da81a539bc361d
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:36:46 +0500

    Add files via upload

---
+++ 10.Adding Comments.ts
@@ -0,0 +1,31 @@
+// Program Name: Greeting Program
+// Author: Umer
+// Date: September 16, 2023
+// Description: This program greets the user with a personalized message.
+
+// Store the user's name in a variable
+const userName: string = "Alice";
+
+// Create a greeting message
+const greetingMessage: string = `Hello, ${userName}! Welcome to our website.`;
+
+// Print the greeting message
+console.log(greetingMessage);
+
+
+                                                  //program 2
+
+  // Program Name: Math Operations
+// Author: Umer
+// Date: September 16, 2023
+// Description: This program performs basic math operations and prints the results.
+
+// Addition operation
+const additionResult: number = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+
+// Subtraction operation
+const subtractionResult: number = 10 - 3;
+console.log(`Subtraction Result: ${subtractionResult}`);
+
+                                                
