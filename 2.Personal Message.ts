df544622a6d45ae990f689d3bd6c63ebf491800a
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:32:41 +0500

    Add files via upload

---
+++ 2.Personal Message.ts
@@ -0,0 +1,14 @@
+// Store the person's name in a variable
+const personName: string = "Alia";
+
+// Create a message using the person's name
+const message: string = `Hello ${personName}, would you like to learn some Python today?`;
+
+// Print the message
+console.log(message);
+
+
+
+
+
+
