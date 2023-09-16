4a9f31db3a28cf72a3b0b68ae4729c227de30a2a
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:37:20 +0500

    Add files via upload

---
+++ 13.Your own array.ts
@@ -0,0 +1,7 @@
+// Create a list of favorite modes of transportation
+const favoriteTransportation: string[] = ["Honda motorcycle", "Tesla Model 3", "Ford Mustang", "Yamaha YZF-R6", "Jeep Wrangler"];
+
+// Print statements about each item in the list
+favoriteTransportation.forEach((item) => {
+  console.log(`I would like to own a ${item}.`);
+});
