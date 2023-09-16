caec371725296b542046d72d180cb448597c138b
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:33:35 +0500

    Add files via upload

---
+++ 4.quote from fvrt person.ts
@@ -0,0 +1,9 @@
+// Store the quote and its author
+const prophetQuote: string = "The best among you are those who have the best manners and character.";
+const author: string = "Prophet Muhammad (peace be upon him)";
+
+// Create the output message
+const output: string = `"${prophetQuote}" - ${author}`;
+
+// Print the output
+console.log(output);
