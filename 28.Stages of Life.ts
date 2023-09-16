962e62eaa368174abf813d83116230ffe76b461b
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:44:33 +0500

    Add files via upload

---
+++ 28.Stages of Life.ts
@@ -0,0 +1,141 @@
+let age = 25; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age < 13) {
+  console.log("You are a child.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else if (age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+//If the person is less than 2 years old, print a message that the person is a baby.
+let age = 1; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age < 13) {
+  console.log("You are a child.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else if (age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+
+//If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+
+let age = 3; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age >= 2 && age < 4) {
+  console.log("You are a toddler.");
+} else if (age < 13) {
+  console.log("You are a child.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else if (age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+
+
+
+// If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+
+let age = 8; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age >= 2 && age < 4) {
+  console.log("You are a toddler.");
+} else if (age >= 4 && age < 13) {
+  console.log("You are a kid.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else if (age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+//If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+
+let age = 16; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age >= 2 && age < 4) {
+  console.log("You are a toddler.");
+} else if (age >= 4 && age < 13) {
+  console.log("You are a kid.");
+} else if (age >= 13 && age < 20) {
+  console.log("You are a teenager.");
+} else if (age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+// If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+
+let age = 30; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age >= 2 && age < 4) {
+  console.log("You are a toddler.");
+} else if (age >= 4 && age < 13) {
+  console.log("You are a kid.");
+} else if (age >= 13 && age < 20) {
+  console.log("You are a teenager.");
+} else if (age >= 20 && age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a senior citizen.");
+}
+
+
+
+
+
+// If the person is age 65 or older, print a message that the person is an elder.
+
+let age = 70; // You can change the age here
+
+if (age < 2) {
+  console.log("You are a baby.");
+} else if (age >= 2 && age < 4) {
+  console.log("You are a toddler.");
+} else if (age >= 4 && age < 13) {
+  console.log("You are a kid.");
+} else if (age >= 13 && age < 20) {
+  console.log("You are a teenager.");
+} else if (age >= 20 && age < 65) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are an elder.");
+}
