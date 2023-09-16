420c78b476d60912f3990b685b7455a4650db631
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:56:13 +0500

    Add files via upload

---
+++ 41.magician.ts
@@ -0,0 +1,13 @@
+function show_magicians(magicians: string[]) {
+    for (let magician of magicians) {
+      console.log(magician);
+    }
+  }
+  
+  // Create an array of magician's names
+  const magicianNames = ["David Copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+  
+  // Call the function to show the magician's names
+  show_magicians(magicianNames);
+
+  
