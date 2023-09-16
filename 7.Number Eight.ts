06ccf26ef4abd643dfb28537341b8a321dbb9faf
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:36:09 +0500

    Add files via upload

---
+++ 7.Number Eight.ts
@@ -0,0 +1,15 @@
+// Addition operation
+const additionResult: number = 6 + 2;
+console.log(`Addition Result: ${additionResult}`);
+
+// Subtraction operation
+const subtractionResult: number = 16 - 8;
+console.log(`Subtraction Result: ${subtractionResult}`);
+
+// Multiplication operation
+const multiplicationResult: number = 4 * 2;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+
+// Division operation
+const divisionResult: number = 64 / 8;
+console.log(`Division Result: ${divisionResult}`);
