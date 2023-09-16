7c6ef8a3252032f5058195b392d3652ebe0997b4
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:36:58 +0500

    Add files via upload

---
+++ 11.Names.ts
@@ -0,0 +1,10 @@
+// Store the names of your friends in an array
+const names: string[] = ["Ali", "Umer", "Uzair", "Umair"];
+
+// Print each person's name one at a time
+console.log("My Friends' Names:");
+
+// Access and print each name using a loop
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
