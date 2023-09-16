78f8ccb5f1e0877dde96192e1bd4bd27492e2056
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:44:14 +0500

    Add files via upload

---
+++ 26.Alien color#2.ts
@@ -0,0 +1,67 @@
+let alien_color = 'yellow'; // You can change the color here
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else if (alien_color === 'yellow') {
+  console.log("You just earned 10 points for shooting down a yellow alien.");
+} else if (alien_color === 'red') {
+  console.log("You just earned 15 points for shooting down a red alien.");
+} else {
+  console.log("Unknown alien color. No points earned.");
+}
+
+
+
+
+
+
+// If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else {
+  console.log("You didn't earn any points for shooting this alien.");
+}
+
+
+
+
+
+
+
+//If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+let alien_color = 'yellow'; // You can change the color here
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+
+
+
+
+
+// Write one version of this program that runs the if block and another that runs the else block.
+
+//version 1(run the if block)
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else {
+  console.log("You didn't earn any points for shooting this alien.");
+}
+
+
+//version 2(run the else block)
+let alien_color = 'red';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
