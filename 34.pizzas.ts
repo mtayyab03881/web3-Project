bafcc435d565f604e562cd0582e8d1c0c2f5e6e0
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:45:34 +0500

    Add files via upload

---
+++ 34.pizzas.ts
@@ -0,0 +1,30 @@
+let favoritePizzas = ["Margherita", "Pepperoni", "Mushroom"];
+
+for (let i = 0; i < favoritePizzas.length; i++) {
+  console.log(`One of my favorite pizzas is ${favoritePizzas[i]}.`);
+}
+
+
+// Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+let favoritePizzas = ["Margherita", "Pepperoni", "Mushroom"];
+
+for (let i = 0; i < favoritePizzas.length; i++) {
+  console.log(`I like ${favoritePizzas[i]} pizza.`);
+}
+
+
+
+//Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+let favoritePizzas = ["Margherita", "Pepperoni", "Mushroom"];
+
+for (let i = 0; i < favoritePizzas.length; i++) {
+  console.log(`I like ${favoritePizzas[i]} pizza.`);
+}
+
+console.log("I really love pizza!");
+
+
+
+
