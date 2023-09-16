34a2cfdad3d3bf2bbd574decf57e1ac8e6a2cbda
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:37:30 +0500

    Add files via upload

---
+++ 14.Guest list.ts
@@ -0,0 +1,7 @@
+// Create a list of people to invite to dinner
+const dinnerGuests: string[] = ["Alia", "Umer", "Areeb"];
+
+// Print invitation messages to each person
+dinnerGuests.forEach((guest) => {
+  console.log(`Dear ${guest}, you are invited to dinner at my place this Saturday at 7 PM. We hope to see you there!`);
+});
