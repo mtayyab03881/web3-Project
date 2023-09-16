c902c00682cff197acc06961916263eb3c34efea
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:44:41 +0500

    Add files via upload

---
+++ 29.Favourite Fruit.ts
@@ -0,0 +1,57 @@
+let favoriteFruits = ["apple", "banana", "mango", "strawberry", "kiwi"];
+let fruitToCheck = "mango"; // You can change the fruit here
+
+if (favoriteFruits.includes(fruitToCheck)) {
+  console.log(`I love ${fruitToCheck}s!`);
+}
+
+// Check for another fruit
+fruitToCheck = "orange"; // You can change the fruit here
+
+if (favoriteFruits.includes(fruitToCheck)) {
+  console.log(`I love ${fruitToCheck}s!`);
+}
+
+// Check for yet another fruit
+fruitToCheck = "blueberry"; // You can change the fruit here
+
+if (favoriteFruits.includes(fruitToCheck)) {
+  console.log(`I love ${fruitToCheck}s!`);
+}
+
+
+
+
+
+
+//Make a array of your three favorite fruits and call it favorite_fruits.
+
+let favorite_fruits = ["apple", "banana", "strawberry"];
+
+
+
+
+
+//Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let favorite_fruits = ["apple", "banana", "strawberry"];
+
+if (favorite_fruits.includes("apple")) {
+  console.log("You really like apples!");
+}
+
+if (favorite_fruits.includes("banana")) {
+  console.log("You really like bananas!");
+}
+
+if (favorite_fruits.includes("strawberry")) {
+  console.log("You really like strawberries!");
+}
+
+if (favorite_fruits.includes("kiwi")) {
+  console.log("You really like kiwis!");
+}
+
+if (favorite_fruits.includes("mango")) {
+  console.log("You really like mangoes!");
+}
