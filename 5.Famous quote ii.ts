c3c5cc68d97c09e9ca453a88ccc0c8b8fffe2af6
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:33:48 +0500

    Add files via upload

---
+++ 5.Famous quote ii.ts
@@ -0,0 +1,12 @@
+// Store the famous person's name in a variable
+const famous_person: string = "Prophet Muhammad (peace be upon him)";
+
+// Store the quote attributed to the famous person
+const prophetQuote: string = "The best among you are those who have the best manners and character.";
+
+// Compose the message
+const message: string = `"${prophetQuote}" - ${famous_person}`;
+
+// Print the message
+console.log(message);
+
