94950e9caf1f18da1b1f57ba52644b8b263519b1
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:44:01 +0500

    Add files via upload

---
+++ 25.Alien colors.ts
@@ -0,0 +1,33 @@
+let alien_color = 'green';
+
+
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
+
+
+
+
+// Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+//version 1 (passes the if test)
+
+
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
+
+
+//Version 2 (Fails the if test):
+
+let alien_color = 'red'; // Changed to 'red'
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
