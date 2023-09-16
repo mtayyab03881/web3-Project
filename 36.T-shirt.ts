71831066cc9c5c924b4b65c663d615020660e907
mtayyab03881 <125695467+mtayyab03881@users.noreply.github.com>
2023-09-16 21:45:54 +0500

    Add files via upload

---
+++ 36.T-shirt.ts
@@ -0,0 +1,7 @@
+function make_shirt(size: string, message: string) {
+    console.log(`A ${size}-sized shirt will be printed with the message: "${message}"`);
+  }
+  
+  // Calling the function
+  make_shirt("Medium", "Hello, World!");
+  
